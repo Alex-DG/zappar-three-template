a3f725ac6c3c7eff610898f647ea630a45f4e4cd
Alex Di Guida <alexandre.diguida@gmail.com>
2026-01-16 14:16:29 +0900

    first commit

---
+++ vite.config.js
@@ -0,0 +1,43 @@
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+
+const htmlPlugin = (env) => {
+    return {
+        name: 'html-transform',
+        transformIndexHtml: (html) =>
+            html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+    }
+}
+
+export default defineConfig(({ mode }) => {
+    return {
+        base: './',
+        assetsInclude: [
+            '**/*.glb',
+            '**/*.gltf',
+            '**/*.fbx',
+            '**/*.mp4',
+            '**/*.webp',
+            '**/*.png',
+            '**/*.jpg',
+        ],
+        optimizeDeps: {
+            exclude: ['@zappar/zappar-threejs', '@zappar/zappar-cv'],
+            include: ['ua-parser-js'],
+        },
+        plugins: [
+            basicSsl(),
+            htmlPlugin(loadEnv(mode, '.')),
+            viteStaticCopy({
+                targets: [
+                    {
+                        src: 'node_modules/@zappar/zappar-cv/lib/zappar-cv.wasm',
+                        dest: 'files',
+                    },
+                ],
+            }),
+        ],
+    }
+})
